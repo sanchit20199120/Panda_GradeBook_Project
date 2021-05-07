17c25f205834e65d05edf6f61df0012fac3d4545
sanchit20199120 <sanchit.bhard@gmail.com>
2021-05-07 13:31:24 -0400

    Loading and Transforming Data

--- GradeBook.py
+++ GradeBook.py
@@ -2,26 +2,26 @@ import pandas as pd
 from pathlib import Path
 
 Dir=Path(__file__).parent
-print(Dir)
 Data_file=Dir/"Data"
-print(Data_file)
-
-df1=pd.read_csv(Data_file/"roster.csv")
-print(df1)
-
-df2=pd.read_csv(Data_file/"hw_exam_grades.csv")
-print(df2)
 
 
-df3=pd.DataFrame()
+roster = pd.read_csv(Data_file/"roster.csv",
+                     converters={"NetID": str.lower, "Email Address": str.lower},
+                     usecols=["Section", "NetID", "Email Address"],
+                     index_col="NetID")
+
+hw_exam_grades=pd.read_csv(Data_file/"hw_exam_grades.csv",
+                           converters={"SID":str.lower},
+                           usecols=lambda x:"Submission" not in x,
+                           index_col="SID")
+quiz_grades=pd.DataFrame()
 for file_path in Data_file.glob('quiz_*_grades.csv'):
-    print(file_path)
-    grade_name = " ".join(file_path.stem.title().split("_")[:2])
-    print(grade_name)
-    df4 = pd.read_csv(file_path,
-                      index_col=["Email"],
-                    usecols=["Email","Grade"]).rename(columns={"Grade":grade_name})
 
-    df3=pd.concat([df3,df4],axis=1,sort=True)
-print(df3)
+    quiz_name = " ".join(file_path.stem.title().split("_")[:2])
 
+    quiz = pd.read_csv(file_path,
+                       converters={"Email": str.lower},
+                       index_col=["Email"],
+                       usecols=["Email", "Grade"]).rename(columns={"Grade": quiz_name})
+
+    quiz_grades=pd.concat([quiz_grades, quiz], axis=1, sort=True)
